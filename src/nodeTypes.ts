565c6c16a26f40831d81f4c9cd7877cdae84fd2a
Eric Newton <ericnewton2251@gmail.com>
2026-05-17 14:14:18 -0700

    Add functions and node types

--- src/nodeTypes.ts
+++ src/nodeTypes.ts
@@ -1,3 +1,5 @@
+import { SyntaxNode } from "./parser";
+
 type ElementsSource = { kind: 'named-children' };
 
 export interface NodeTypeDescriptor {
@@ -5,6 +7,40 @@ export interface NodeTypeDescriptor {
     openToken: string;
     closeToken: string;
     separator: string;
-    bracketSpace: boolean;
+    bracketSpacing: boolean;
     elementsField: ElementsSource;
+}
+
+type SupportedNodeTypes = 'array' | 'object';
+type SupportedSyntaxNode = SyntaxNode & { type: SupportedNodeTypes };
+
+export const NODE_TYPES: Record<SupportedNodeTypes, NodeTypeDescriptor> = {
+    array: {
+        type: 'array',
+        openToken: '[',
+        closeToken: ']',
+        separator: ',',
+        bracketSpacing: false,
+        elementsField: { kind: 'named-children' }
+    },
+    object: {
+        type: 'object',
+        openToken: '{',
+        closeToken: '}',
+        separator: ',',
+        bracketSpacing: true,
+        elementsField: { kind: 'named-children' }
+    }
+} as const;
+
+export function isSupported(node: SyntaxNode): node is SupportedSyntaxNode {
+    return node.type in NODE_TYPES;
+}
+
+export function descriptorFor(node: SyntaxNode): NodeTypeDescriptor | undefined {
+    if (isSupported(node)) {
+        return NODE_TYPES[node.type];
+    }
+
+    return undefined;
 }
