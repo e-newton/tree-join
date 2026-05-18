1bb698abb776b8c11baccd7ae0a9705f5277ac2b
Eric Newton <ericnewton2251@gmail.com>
2026-05-17 17:51:06 -0700

    Find target node from position

---
+++ src/select.ts
@@ -0,0 +1,13 @@
+import { isSupported } from "./nodeTypes";
+import { Point, SyntaxNode, Tree } from "./parseSource";
+
+export function findTarget(tree: Tree, postion: Point): SyntaxNode | undefined
+{
+   let smallestNamedNode = tree.rootNode.descendantForPosition(postion);
+
+   while (smallestNamedNode && !isSupported(smallestNamedNode)) {
+    smallestNamedNode = smallestNamedNode.parent;
+   }
+
+   return smallestNamedNode ?? undefined;
+}
